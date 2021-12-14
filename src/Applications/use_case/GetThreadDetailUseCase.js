33098429e594b15d092867e098e889824ffd3eab
Ridwan <ridwan.rais2@gmail.com>
2021-12-14 13:25:55 +0700

    refactored GetThreadDetailUseCase

--- src/Applications/use_case/GetThreadDetailUseCase.js
+++ src/Applications/use_case/GetThreadDetailUseCase.js
@@ -16,76 +16,49 @@ class GetThreadDetailUseCase {
     const { threadId } = useCasePayload;
 
     const thread = await this._threadRepository.getThread(threadId);
-    const threadDetail = new ThreadDetail({
-      ...thread,
-      comments: [],
-    });
 
     const comments = await this._commentRepository.getCommentsByThreadId(
       threadId
     );
-    // const comments = this.sortByDate(UnorderedComments);
 
     const commentIds = comments.map((comment) => comment.id);
     const bulkReplies = await this._replyRepository.getRepliesByCommentIds(
       commentIds
     );
-    // const bulkReplies = this.sortByDate(UnorderedBulkReplies);
 
-    let i = 0;
-    while (i < comments.length) {
-      const comment = comments[i];
-
-      const { id, content, date, username, is_delete } = comment;
-      const commentDetail = new CommentDetail({
-        id,
-        content,
-        date,
-        username,
-        isDeleted: is_delete,
-        replies: [],
-      });
-
-      if (commentDetail.isDeleted) {
-        commentDetail.content = "**komentar telah dihapus**";
-      }
-
-      const replies = bulkReplies.filter(
-        (reply) => reply.comment_id === comment.id
-      );
-
-      let j = 0;
-      while (j < replies.length) {
-        const reply = replies[j];
-
-        const { id, content, date, username, is_delete } = reply;
-        const replyDetail = new ReplyDetail({
-          id,
-          content,
-          date,
-          username,
-          isDeleted: is_delete,
-        });
-
-        if (replyDetail.isDeleted) {
-          replyDetail.content = "**balasan telah dihapus**";
-        }
-
-        commentDetail.replies.push(replyDetail);
-        j += 1;
-      }
-
-      threadDetail.comments.push(commentDetail);
-      i += 1;
-    }
-
-    return threadDetail;
+    return new ThreadDetail({
+      ...thread,
+      comments: this.getCommentsAndReplies(comments, bulkReplies),
+    });
   }
 
-  sortByDate(commentsOrRepliesArray) {
-    return commentsOrRepliesArray.sort(
-      (a, b) => new Date(a.date) - new Date(b.date)
+  getCommentsAndReplies(comments, bulkReplies) {
+    return comments.map(
+      (comment) =>
+        new CommentDetail({
+          ...comment,
+          isDeleted: comment.is_delete,
+          content: comment.is_delete
+            ? "**komentar telah dihapus**"
+            : comment.content,
+          replies: this.getRepliesForComment(bulkReplies, comment.id),
+        })
     );
+  }
+
+  getRepliesForComment(bulkReplies, commentId) {
+    return bulkReplies
+      .filter((reply) => reply.comment_id === commentId)
+      .map(
+        (reply) =>
+          new ReplyDetail({
+            ...reply,
+            isDeleted: reply.is_delete,
+            content: reply.is_delete
+              ? "**balasan telah dihapus**"
+              : reply.content,
+          })
+      );
   }
 }
 
